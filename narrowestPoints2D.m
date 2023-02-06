1761d83dc0d2cd0bde67ccd8e83e98357f6e4399
JanMrogala <jan.mrogala@seznam.cz>
2023-02-06 07:58:15 +0100

    update

--- narrowestPoints2D.m
+++ narrowestPoints2D.m
@@ -41,6 +41,36 @@ function ret = narrowestPoint2D(image, fillColor, boundaryColor, seedPos)
 
   endfor
 
+  #x direction --------------
+    temp = image;
+    dilatedImage = dilation(temp, structuringElementX, boundaryColor, seedPos);
+
+    for x = 1:(size(image)(1,2)-1)
+
+      filled = lineSeedFill2D(seedPos(1,1), seedPos(1,2), fillColor, boundaryColor, dilatedImage);
+
+      dilatedFilledArea = dilation(filled, structuringElementOppX, fillColor, seedPos);
+      dilatedFilledArea2 = dilation(filled, structuringElement, fillColor, seedPos);
+
+      vec1 = convertPixelsToVectors(dilatedFilledArea, fillColor);
+      vec2 = convertPixelsToVectors(dilatedFilledArea2, fillColor);
+      differenceVectors = difference(vec1, vec2)
+      image2 = image;
+
+      for i = 1:columns(differenceVectors)
+        if(temp(differenceVectors{i}(1,2),differenceVectors{i}(1,1),:) != boundaryColor)
+          narrowestPointsX(end+1) = differenceVectors{i};
+        endif
+      endfor
+
+      if(size(narrowestPointsX)(1,1) > 0)
+        break
+      endif
+      temp = dilatedImage;
+      dilatedImage = dilation(dilatedImage, structuringElementX, boundaryColor, seedPos);
+
+    endfor
+
 ret = cat(2,narrowestPointsX, narrowestPointsY);
 
 endfunction
